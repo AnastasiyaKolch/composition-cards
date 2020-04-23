50b1509722acf564bd703497236b808f947da931
AnastasiyaKolch <45988412+AnastasiyaKolch@users.noreply.github.com>
2020-04-23 17:27:08 +0500

    +

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import Cards from '../src/components/Cards'
+import './App.css';
+import Card from './components/Card';
 
-export default function App() {
+function App() {
+  
+const card1 = {
+  title: 'Card title',
+  text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+  href: '#'
+};
+
+const card2 = {
+  title: 'Special title treatment',
+  text: 'With supporting text below as a natural lead-in to additional content.',
+  href: '#'
+};
+
   return (
-    <Cards />
+    <React.Fragment>
+      <Card {...card1}><img src="https://cdn.pixabay.com/photo/2013/10/11/16/43/roses-194110_960_720.jpg" className="card-img-top" alt="" /></Card>
+      <Card {...card2}/>
+    </React.Fragment>
+    
   );
 }
+
+export default App;
--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-
-export default function Content(props) {
-
-    return (
-        <button href="#" className="btn btn-primary">{props.children}</button>
-    )
-}
